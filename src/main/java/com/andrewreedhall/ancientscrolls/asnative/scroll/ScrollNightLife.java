9404466e42b6802d4c29dc49d61f1081b6f53bd5
Camo5hark <camo5hark10@gmail.com>
2025-09-10 00:41:27 -0400

    license boilerplate headers

--- src/main/java/com/andrewreedhall/ancientscrolls/asnative/scroll/ScrollNightLife.java
+++ src/main/java/com/andrewreedhall/ancientscrolls/asnative/scroll/ScrollNightLife.java
@@ -1,3 +1,24 @@
+/*
+AncientScrolls SpigotMC plugin
+Copyright (C) 2025  Andrew Hall
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+License file: <project-root>/COPYING
+GitHub repo URL: www.github.com/Camo5hark/AncientScrolls
+ */
+
 package com.andrewreedhall.ancientscrolls.asnative.scroll;
 
 import com.andrewreedhall.ancientscrolls.util.CommonSets;
@@ -14,7 +35,6 @@ public final class ScrollNightLife extends ItemScrollNative implements Listener 
         });
         this.enderDragonReward = true;
         this.special = true;
-        this.putMCLootTableGenProb("chests/end_city_treasure", 0.131);
         this.addPotionEffectToEquippingPlayers(NIGHT_VISION_POTION_EFFECT, (final Player equippingPlayer) -> !equippingPlayer.getWorld().isDayTime());
     }
 
