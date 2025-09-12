dfc2c7b6e6c496151bfd51ddbcb65cce3dd4b419
Camo5hark <camo5hark10@gmail.com>
2025-09-11 20:01:37 -0400

    pet warden targets entity that damages owner
    pet warden metadata

--- src/main/java/com/andrewreedhall/ancientscrolls/asnative/scroll/ScrollWardenTaming.java
+++ src/main/java/com/andrewreedhall/ancientscrolls/asnative/scroll/ScrollWardenTaming.java
@@ -1,6 +1,9 @@
 package com.andrewreedhall.ancientscrolls.asnative.scroll;
 
 import io.papermc.paper.event.entity.WardenAngerChangeEvent;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Warden;
@@ -8,14 +11,23 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityPotionEffectEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.List;
+
+import static com.andrewreedhall.ancientscrolls.AncientScrollsPlugin.plugin;
 
 public final class ScrollWardenTaming extends ItemScrollNative implements Listener {
+    private static final String PDK_PET_WARDEN = "scroll_warden_taming_pet_warden";
+    private static final int PET_WARDEN_TTL = 600;
+
     public ScrollWardenTaming() {
         super("warden_taming", "Warden Taming", new String[] {
                 "Negates darkness",
-                "Wardens will not target user",
-                "Summons a warden when attacked"
+                "Summons a player-friendly warden when attacked"
         });
         this.special = true;
         this.putMCLootTableGenProb("entities/warden", 1.0);
@@ -34,13 +46,46 @@ public final class ScrollWardenTaming extends ItemScrollNative implements Listen
         ) {
             return;
         }
-        final Warden damagedPlayerPetWarden = damagingLivingEntity.getWorld().spawn(damagingLivingEntity.getLocation(), Warden.class);
-        damagedPlayerPetWarden.setPersistent(false);
+        final List<MetadataValue> damagedPlayerPetWardenData = damagedPlayer.getMetadata(PDK_PET_WARDEN);
+        Warden damagedPlayerPetWarden;
+        if (damagedPlayerPetWardenData.isEmpty()) {
+            // summon warden
+            damagedPlayerPetWarden = damagingLivingEntity.getWorld().spawn(damagingLivingEntity.getLocation(), Warden.class);
+            damagedPlayer.setMetadata(PDK_PET_WARDEN, new FixedMetadataValue(plugin(), damagedPlayerPetWarden));
+            damagedPlayerPetWarden.setPersistent(false);
+            damagedPlayerPetWarden.setMetadata(PDK_PET_WARDEN, new FixedMetadataValue(plugin(), true));
+            plugin().scheduleTask((final BukkitScheduler scheduler) -> scheduler.scheduleSyncDelayedTask(
+                    plugin(),
+                    () -> {
+                        damagedPlayer.removeMetadata(PDK_PET_WARDEN, plugin());
+                        damagedPlayerPetWarden.remove();
+                        final World damagedPlayerPetWardenWorld = damagedPlayerPetWarden.getWorld();
+                        damagedPlayerPetWardenWorld.playSound(damagedPlayerPetWarden, Sound.BLOCK_BEACON_DEACTIVATE, 1.0F, 0.75F);
+                        damagedPlayerPetWardenWorld.spawnParticle(
+                                Particle.SOUL_FIRE_FLAME,
+                                damagedPlayerPetWarden.getEyeLocation(),
+                                50,
+                                1.0,
+                                1.0,
+                                1.0,
+                                0.1
+                        );
+                    },
+                    PET_WARDEN_TTL
+            ));
+        } else {
+            damagedPlayerPetWarden = (Warden) damagedPlayerPetWardenData.getFirst().value();
+            if (damagedPlayerPetWarden == null) {
+                return;
+            }
+            damagedPlayerPetWarden.setTarget(damagingLivingEntity);
+            damagedPlayerPetWarden.setAnger(damagingLivingEntity, 100);
+        }
     }
 
     @EventHandler
     public void onWardenAngerChange(final WardenAngerChangeEvent event) {
-        if (!(event.getTarget() instanceof Player targettedPlayer) || !this.isEquipping(targettedPlayer)) {
+        if (!event.getEntity().hasMetadata(PDK_PET_WARDEN) || !(event.getTarget() instanceof Player)) {
             return;
         }
         event.setCancelled(true);
