11d43565017c8a74435c8c946ec309f52fb9f402
Camo5hark <camo5hark10@gmail.com>
2025-09-13 13:34:50 -0400

    partition scrolls into lists of 45, add to corresponding page inventory

--- src/main/java/com/andrewreedhall/ancientscrolls/item/scroll/GUIInventoryHandler.java
+++ src/main/java/com/andrewreedhall/ancientscrolls/item/scroll/GUIInventoryHandler.java
@@ -1,16 +1,29 @@
 package com.andrewreedhall.ancientscrolls.item.scroll;
 
+import com.google.common.collect.Lists;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public final class GUIInventoryHandler implements Listener {
-    private static final int SCROLLS_PER_PAGE = 45;
+import static com.andrewreedhall.ancientscrolls.AncientScrollsPlugin.plugin;
 
-    private final List<Inventory> guiInventoryPages = new ArrayList<>();
+public final class GUIInventoryHandler implements Listener {
+    private final List<Inventory> guiPageInventories = new ArrayList<>();
 
     public GUIInventoryHandler() {
+        Lists.partition(ItemScroll.createListOfAllRegistered(), 45).forEach((final List<ItemScroll> scrolls) -> {
+            final Inventory guiPageInventory = plugin().getServer().createInventory(null, 54);
+            guiPageInventory.addItem(
+                    scrolls
+                            .stream()
+                            .map((final ItemScroll scroll) -> scroll.createItemStack(1))
+                            .toArray(ItemStack[]::new)
+            );
+            // TODO add control items
+            this.guiPageInventories.add(guiPageInventory);
+        });
     }
 }
