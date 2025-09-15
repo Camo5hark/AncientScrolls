e610884a0e1a23e7ceca8e13109ce5e0c20459c9
Camo5hark <camo5hark10@gmail.com>
2025-09-14 22:47:48 -0400

    added license boilerplate

--- src/main/java/com/andrewreedhall/ancientscrolls/asnative/scroll/ScrollHypermobility.java
+++ src/main/java/com/andrewreedhall/ancientscrolls/asnative/scroll/ScrollHypermobility.java
@@ -1,3 +1,24 @@
+/*
+AncientScrolls SpigotMC plugin
+Copyright (C) 2025  Andrew Hall
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+License file: <project-root>/COPYING
+GitHub repo URL: www.github.com/Camo5hark/AncientScrolls
+ */
+
 package com.andrewreedhall.ancientscrolls.asnative.scroll;
 
 import org.bukkit.attribute.Attribute;
