ba66900f4d0808ace29f1c51cddae04afd4bc576
Camo5hark <camo5hark10@gmail.com>
2025-09-16 21:00:48 -0400

    began npc dev

--- src/main/java/com/andrewreedhall/ancientscrolls/npc/AncientScrollsNPC.java
+++ src/main/java/com/andrewreedhall/ancientscrolls/npc/AncientScrollsNPC.java
@@ -22,13 +22,10 @@ GitHub repo URL: www.github.com/Camo5hark/AncientScrolls
 package com.andrewreedhall.ancientscrolls.npc;
 
 import com.andrewreedhall.ancientscrolls.AncientScrollsRegistry;
-import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 
 public abstract class AncientScrollsNPC extends AncientScrollsRegistry.Value {
-    public AncientScrollsNPC(NamespacedKey key) {
+    public AncientScrollsNPC(final NamespacedKey key) {
         super(key);
     }
-
-    public abstract NPCInstance spawn(Location location);
 }
