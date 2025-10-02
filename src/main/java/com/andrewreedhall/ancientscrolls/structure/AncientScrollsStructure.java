13bfe53a67109db414d5ff99de81eea053cf5bc5
Camo5hark <camo5hark10@gmail.com>
2025-10-02 15:02:35 -0400

    generate one structure per chunk

--- src/main/java/com/andrewreedhall/ancientscrolls/structure/AncientScrollsStructure.java
+++ src/main/java/com/andrewreedhall/ancientscrolls/structure/AncientScrollsStructure.java
@@ -1,3 +1,24 @@
+/*
+AncientScrolls SpigotMC plugin
+Copyright (C) 2025  Andrew Hall
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+License file: <project-root>/COPYING
+GitHub repo URL: www.github.com/Camo5hark/AncientScrolls
+ */
+
 package com.andrewreedhall.ancientscrolls.structure;
 
 import com.andrewreedhall.ancientscrolls.AncientScrollsRegistry;
@@ -12,11 +33,12 @@ public abstract class AncientScrollsStructure extends AncientScrollsRegistry.Val
 
     protected abstract boolean canGenerate(final Chunk chunk);
 
-    public void generate(final ChunkLoadEvent event) {
+    public boolean generate(final ChunkLoadEvent event) {
         final Chunk chunk = event.getChunk();
         if (!this.canGenerate(chunk)) {
-            return;
+            return false;
         }
         // generate
+        return true;
     }
 }
--- src/main/java/com/andrewreedhall/ancientscrolls/structure/StructureListener.java
+++ src/main/java/com/andrewreedhall/ancientscrolls/structure/StructureListener.java
@@ -1,12 +1,40 @@
+/*
+AncientScrolls SpigotMC plugin
+Copyright (C) 2025  Andrew Hall
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+License file: <project-root>/COPYING
+GitHub repo URL: www.github.com/Camo5hark/AncientScrolls
+ */
+
 package com.andrewreedhall.ancientscrolls.structure;
 
+import com.andrewreedhall.ancientscrolls.util.Randomizer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import static com.andrewreedhall.ancientscrolls.AncientScrollsPlugin.plugin;
 
 public final class StructureListener implements Listener {
+    private static final Comparator<AncientScrollsStructure> STRUCTURE_RANDOMIZER = new Randomizer<>();
+
     public StructureListener() {}
 
     @EventHandler
@@ -14,6 +42,12 @@ public final class StructureListener implements Listener {
         if (!event.isNewChunk()) {
             return;
         }
-        plugin().getStructureRegistry().getAll().forEach((final AncientScrollsStructure structure) -> structure.generate(event));
+        final List<AncientScrollsStructure> registeredStructures = new ArrayList<>(plugin().getStructureRegistry().getAll());
+        registeredStructures.sort(STRUCTURE_RANDOMIZER);
+        for (final AncientScrollsStructure structure : registeredStructures) {
+            if (structure.generate(event)) {
+                break;
+            }
+        }
     }
 }
